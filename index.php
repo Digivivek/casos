2cdca61f03d7d9feedc3706508e03ec46f946c92
Vivek Kumar <105774765+Digivivek@users.noreply.github.com>
2025-12-06 16:58:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-// guard: avoid redirect/self nesting via ?p=index.php
 if (isset($_GET['p']) && stripos($_GET['p'], 'index.php') !== false) {
     unset($_GET['p']);
 }
@@ -13,8 +12,6 @@ $options = array();
 $tarp = new TARPLoader($options);
 $tarp->excute();
 
-$GLOBALS['_ta_rp_key'] = 'a55b83cff99869f511c919de320141e4';
-$GLOBALS['_ta_reverse_proxy_id'] = '04c8dn';
 
 require 'bootloader_218fc78d91335bef8bd0b3f91ce806e3.php';
 
